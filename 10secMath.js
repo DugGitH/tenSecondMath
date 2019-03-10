64ea51145299190bc0e1e8bda1363a4449d18a01
Michael <mictalle@gmail.com>
2019-03-09 20:25:53 -0800

    Added javascript function for slider and high score

--- 10secMath.js
+++ 10secMath.js
@@ -43,7 +43,95 @@ $(document).ready(function(){
     }
   };
   
- 
-  
-});
+  var slider = document.getElementById("myRange");
+  var output = document.getElementById("demo");
+  output.innerHTML = slider.value;
 
+  slider.oninput = function() {
+    output.innerHTML = this.value;      
+  }
+  
+  //Need to grab number value 
+  number = $("#myRange").val();
+
+  $(document).on('input', '#myRange', function() {
+    number = $(this).val();  
+  })
+ 
+  var randomNumberGenerator = function (size) {
+    return Math.ceil(Math.random() * size);
+  };
+  
+  var questionGenerator = function () {
+    var question = {};
+    var num1 = randomNumberGenerator(number);
+    var num2 = randomNumberGenerator(number);
+        
+    if($("#Add").prop('checked')){
+        question.answer = num1 + num2;
+        question.equation = String(num1) + " + " + String(num2);
+    }
+    
+    if($("#Subtract").prop('checked')){
+        
+        for(var i=0; i<=10; i++){            
+            if(num1>=num2){                
+                break;
+            }
+            else{
+                num1 = randomNumberGenerator(number);
+                i++;
+            }
+        }        
+        
+        question.answer = num1 - num2;
+        question.equation = String(num1) + " - " + String(num2);
+    }
+    
+    if($("#Multiply").prop('checked')){
+        question.answer = num1 * num2;
+        question.equation = String(num1) + " * " + String(num2);
+    }
+    
+    if($("#Divide").prop('checked')){
+        for(i=2; i<=10; i++){
+            dividend=num1%i;
+            if(dividend===0){
+                num2=i;
+                break;
+            }
+            else{
+                num1 = randomNumberGenerator(number);
+                i=2;
+            }
+        }
+        
+        question.answer = num1 / num2;
+        question.equation = String(num1) + " / " + String(num2);
+    }      
+        
+    return question;
+  };
+  
+  var renderNewQuestion = function () {      
+    currentQuestion = questionGenerator();
+    $('#equation').text(currentQuestion.equation);      
+  };
+  
+  var checkAnswer = function (userInput, answer) {
+    if (userInput === answer) {
+      renderNewQuestion();
+      $('#user-input').val('');
+      updateTimeLeft(+1);
+      updateScore(+1);
+    }
+  };
+   
+  $('#user-input').on('keyup', function () {
+    startGame();
+    checkAnswer(Number($(this).val()), currentQuestion.answer);
+    
+  });
+  
+    renderNewQuestion();
+});
